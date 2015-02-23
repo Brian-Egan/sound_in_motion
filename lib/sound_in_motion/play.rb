4c4de2d7bce86b89914ea81d6cd80ccfc0b38368
Brian Egan <BrianEgan@outlook.com>
2015-02-23 14:55:01 -0500

    Cleaning

--- lib/sound_in_motion/play.rb
+++ lib/sound_in_motion/play.rb
@@ -1,14 +1,15 @@
 module SoundInMotion
   class Play
+    # See list of system sounds here: https://github.com/TUNER88/iOSSystemSoundsLibrary
+    # To Do: Add aliases for those system sound id numbers.
 
     def self.local_file(file_name)
         if UIDevice.currentDevice.model.include?("Simulator")
             puts "Sounds will not play in the simulator. Please run on a iDevice."
         else
           sound_id = Pointer.new('I')
-            # local_file_name = NSURL.fileURLWithPath(File.join(NSBundle.mainBundle.resourcePath, which))
-            AudioServicesCreateSystemSoundID(NSURL.fileURLWithPath(File.join(NSBundle.mainBundle.resourcePath, file_name)), sound_id)
-            AudioServicesPlaySystemSound(sound_id[0])
+          AudioServicesCreateSystemSoundID(NSURL.fileURLWithPath(File.join(NSBundle.mainBundle.resourcePath, file_name)), sound_id)
+          AudioServicesPlaySystemSound(sound_id[0])
         end
     end
 
